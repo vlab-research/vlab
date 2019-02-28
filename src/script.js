bbdc82fc25545ed6d0d8293232aff9f87e0bd8a9
Berta Cume <bertacume@gmail.com>
2019-02-28 19:51:46 +0100

    refactor: remove jQuery

--- src/script.js
+++ src/script.js
@@ -2,23 +2,19 @@
 
 const SERVER_URL = 'http://localhost:3001/events';
 
-function handleEvent (data, type) {
-  $.ajax({
-    url: SERVER_URL,
-    type : 'POST',
-    data: JSON.stringify({ data, type }),
-    dataType: 'json',
-    contentType: 'application/json' 
-  });
+function handleEvent(data, eventType) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', SERVER_URL);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({ data, eventType }));
 }
-
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
   const options = {
     id: 280815263,
     width: 800
   };
   const player = new Vimeo.Player('vimeoVideo', options);
-  
+
   player.on('error', data => handleEvent(data, 'error'));
 
   player.on('loaded', data => handleEvent(data, 'loaded'));
