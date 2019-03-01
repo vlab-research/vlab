d26bc315c389dd34361b0156a448487334ff4caa
Nandan <nandanmarkrao@gmail.com>
2019-03-01 18:13:01 +0100

    bump 0.0.10

--- dev.sh
+++ dev.sh
@@ -10,5 +10,5 @@ kubectl create secret generic gbv-bot-envs --from-env-file .env
 
 # App
 kubectl delete -f kube
-docker build -t nandanrao/gbv-replybot:0.0.10 .
+docker build -t nandanrao/gbv-replybot:0.0.11 .
 kubectl apply -f kube
