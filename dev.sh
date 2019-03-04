7b27b7b910397b2026db8d0ea8b4f423c11fb559
Nandan <nandanmarkrao@gmail.com>
2019-03-04 18:57:14 +0100

    changed dev.sh with docker org vlabresearch

--- dev.sh
+++ dev.sh
@@ -10,5 +10,5 @@ kubectl create secret generic gbv-bot-envs --from-env-file .env
 
 # App
 kubectl delete -f kube
-docker build -t nandanrao/gbv-replybot:0.0.14 .
+docker build -t vlabresearch/gbv-replybot:0.0.14 .
 kubectl apply -f kube
