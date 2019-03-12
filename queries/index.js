e5beb13357551fff6c675ae4b5d5e7a7ce59abea
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-03-12 18:06:36 +0100

    feat: add queries system

---
+++ queries/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { Pool } = require('pg')
+
+const basename = path.basename(__filename);
+const { DATABASE_CONFIG } = require('../config');
+
+const db = {};
+
+// Connect to the database through ENV config
+const pool = new Pool(DATABASE_CONFIG);
+pool.on('error', err => { throw err });
+
+// Read all the models in the folder and generate an available set of queries
+fs
+  .readdirSync(__dirname)
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file));
+    db[model.name] = model.queries(pool);
+  });
+
+db.pool = pool;
+
+module.exports = db;
+
+
+
---
+++ queries/users.queries.js
@@ -0,0 +1,13 @@
+'use strict';
+
+async function all () {
+  const { rows } = await this.query('SELECT * FROM messages ORDER BY timestamp ASC');
+  return rows;
+}
+
+module.exports = {
+  name: 'User',
+  queries: (pool) => ({
+    all: all.bind(pool)
+  })
+}
