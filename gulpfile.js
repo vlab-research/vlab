526063ad95cf0d9de6915427cab7ab84661cb49a
Berta Cume <bertacume@gmail.com>
2019-03-13 17:30:15 +0100

    refactor: pass all env varibales to mustache

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 
 gulp.task('replace', function() {
   return gulp.src(['./src/script.js', './src/index.html', './src/style.css'])
-    .pipe(mustache({ SERVER_URL: process.env.SERVER_URL }))
+    .pipe(mustache(process.env))
     .pipe(gulp.dest('./dist'))
 });
