49ea6dc1eb7c2a18d790b24f37401682897ca0b4
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-03-13 23:08:48 +0100

    fix: query and related test

--- queries/users/users.queries.js
+++ queries/users/users.queries.js
@@ -1,9 +1,18 @@
 'use strict';
 
 async function all() {
-  const { rows } = await this.query(
-    'SELECT * FROM messages ORDER BY timestamp ASC',
-  );
+  const GET_ALL = `SELECT *
+    FROM  (
+       SELECT DISTINCT ON (1) userid, timestamp AS first_timestamp, content AS first_message
+       FROM   messages
+       ORDER  BY 1,2
+       ) f
+    JOIN (
+       SELECT DISTINCT ON (1) userid, timestamp AS last_timestamp, content AS second_message
+       FROM   messages
+       ORDER  BY 1,2 DESC
+       ) l USING (userid)`;
+  const { rows } = await this.query(GET_ALL);
   return rows;
 }
 
--- queries/users/users.test.js
+++ queries/users/users.test.js
@@ -45,50 +45,20 @@ describe('User queries', () => {
   });
 
   describe('.all()', () => {
-    it('should get all list of messages', async () => {
-      const mockMessage = '{ "foo": "bar" }';
-      await vlabPool.query(
-        'INSERT INTO messages(id, userid, content, timestamp) values($1, $2, $3, $4)',
-        [100000, '123', mockMessage, new Date()],
-      );
+    it('should get all list of user with the first and last message', async () => {
+      const MOCK_QUERY = `INSERT INTO messages(id, userid, content, timestamp) 
+      VALUES
+        (100001, '124', '{ "text": "last" }', current_date + interval '14 hour')
+       ,(100003, '123', '{ "text": "last" }', current_date + interval '12 hour')
+       ,(100004, '124', '{ "text": "first" }', current_date + interval '10 hour')
+       ,(100005, '123', '{ "text": "first" }', current_date + interval '8 hour')
+       ,(100006, '124', '{ "text": "middle" }', current_date + interval '12 hour')`;
+      await vlabPool.query(MOCK_QUERY);
       const messages = await User.all();
-      messages[0].content.should.equal('{ "foo": "bar" }');
-    });
-
-    it('should return the user as a string', async () => {
-      const mockMessage = '{ "foo": "bar" }';
-      await vlabPool.query(
-        'INSERT INTO messages(id, userid, content, timestamp) values($1, $2, $3, $4)',
-        [100000, '123', mockMessage, new Date()],
-      );
-      const messages = await User.all();
-      messages[0].userid.should.equal('123');
-    });
-
-    it('should gets multiple messages from one user', async () => {
-      const mockMessages = ['{ "foo": "bar" }', '{ "baz": "qux" }'];
-      for (let msg of mockMessages) {
-        await vlabPool.query(
-          'INSERT INTO messages(id, userid, content, timestamp) values($1, $2, $3, $4)',
-          [Math.floor(Math.random() * 1000000), '123', msg, new Date()],
-        );
-      }
-      const messages = await User.all();
-      messages[0].content.should.equal('{ "foo": "bar" }');
-      messages[1].content.should.equal('{ "baz": "qux" }');
-    });
-
-    it('rethrows errors on pool error', async () => {
-      class TestError extends Error {}
-      let error;
-
-      try {
-        vlabPool.emit('error', new TestError('foo'));
-      } catch (e) {
-        error = e;
-      }
-
-      error.should.be.instanceof(TestError);
+      messages[0].first_message.should.equal('{ "text": "first" }');
+      messages[0].second_message.should.equal('{ "text": "last" }');
+      messages[1].first_message.should.equal('{ "text": "first" }');
+      messages[1].second_message.should.equal('{ "text": "last" }');
     });
   });
 });
