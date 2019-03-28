660a8969ad579d3ede9fe7a3266fcee9b2500165
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-03-28 13:02:14 +0100

    feat: add typeform utils (validation)

---
+++ utils/typeform.util.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const joi = require('joi');
+
+class Typeform {
+  static validate(reqData) {
+    const formSchema = joi
+      .object({
+        formid: joi
+          .string()
+          .alphanum()
+          .required(),
+        userid: joi
+          .string()
+          .email()
+          .required(),
+        shortcode: joi
+          .string()
+          .length(4, 'utf8')
+          .regex(/^[0-9]+$/)
+          .required(),
+        form: joi.string().required(),
+      })
+      .unknown()
+      .required();
+
+    const { error } = joi.validate(reqData, formSchema);
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+  }
+}
+
+module.exports = Typeform;
