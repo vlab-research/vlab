9480088212b3998189ffe11b91806a1f3a908f81
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-03-28 12:06:41 +0100

    fix: update index imports

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const responses = require('./responses/response.routes');
+const responses = require('./responses');
+const typeforms = require('./typeforms');
 
-router.use('/responses', responses);
+router.use('/responses', responses).use('/typeforms', typeforms);
 
 module.exports = router;
