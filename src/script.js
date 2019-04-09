ec332c3aa299cbda37bda19f76fad4fde557adff
Berta Cume <bertacume@gmail.com>
2019-04-09 19:04:26 +0200

    chore: install messenger sdk

--- src/script.js
+++ src/script.js
@@ -2,35 +2,40 @@
 
 const SERVER_URL = '{{{SERVER_URL}}}';
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const videoId = params.get('id');
+let psid;
 
 function handleEvent(data, eventType) {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', SERVER_URL);
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({ data, eventType }));
+  xhr.send(JSON.stringify({ data, eventType, psid }));
 }
+
+window.extAsyncInit = function () {
+
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const options = {
-    id,
+    id: videoId,
     width: 800
   };
   const player = new Vimeo.Player('vimeoVideo', options);
 
   player.ready().then(() => {
     player.on('ended', data => handleEvent(data, 'ended'));
-  
+
     player.on('error', data => handleEvent(data, 'error'));
-    
+
     player.on('pause', data => handleEvent(data, 'pause'));
-    
+
     player.on('play', data => handleEvent(data, 'play'));
-    
+
     player.on('playbackratechange', data => handleEvent(data, 'playbackratechange'));
-    
+
     player.on('seeked', data => handleEvent(data, 'seeked'));
-  
+
     player.on('volumechange', data => handleEvent(data, 'volumechange'));
   }).catch((err) => {
     console.error(err);
