2c456dd6e1b07830da68dd43001a6c95b1f24884
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-04-24 16:18:07 +0200

    feat: add Responses cube schema

---
+++ schema/Responses.js
@@ -0,0 +1,57 @@
+/* global cube */
+cube(`Responses`, {
+  sql: `SELECT * FROM responses`,
+
+  measures: {
+    count: {
+      type: `count`,
+    },
+
+    uniqueUserCount: {
+      sql: `userid`,
+      type: `countDistinct`,
+    },
+
+    startTime: {
+      type: `min`,
+      sql: `timestamp`,
+    },
+
+    endTime: {
+      type: `max`,
+      sql: `timestamp`,
+    },
+  },
+
+  dimensions: {
+    formid: {
+      type: `string`,
+      sql: `formid`,
+    },
+
+    userid: {
+      type: `string`,
+      sql: `userid`,
+    },
+
+    flowid: {
+      type: `string`,
+      sql: `flowid`,
+    },
+
+    timestamp: {
+      type: `time`,
+      sql: `timestamp`,
+    },
+
+    response: {
+      type: `string`,
+      sql: `response`,
+    },
+
+    questionId: {
+      type: `string`,
+      sql: `question_idx`,
+    },
+  },
+});
