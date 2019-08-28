f3d35fb8172c70b14aa3c057a09a8d4edcc747d1
Nandan <nandanmarkrao@gmail.com>
2019-08-28 13:45:53 +0200

    updated dev files

--- dev.sh
+++ dev.sh
@@ -1,10 +1,13 @@
 eval $(minikube docker-env)
 
 # Secrets
-kubectl delete secret gbv-bot-envs
-kubectl create secret generic gbv-bot-envs --from-env-file .env
+#kubectl delete secret gbv-bot-envs
+#kubectl create secret generic gbv-bot-envs --from-env-file .env
 
 # App
 kubectl delete -f kube
-docker build -t vlabresearch/facebot:0.0.1 .
+docker build -t localhost:32000/facebot:registry .
+docker push localhost:32000/facebot:registry
 kubectl apply -f kube
+
+# kubelog gbv-facebot 0 --follow
