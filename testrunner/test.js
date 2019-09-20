e2a34d49b89aeb47aa01489d33a958ee3db4007d
Nandan <nandanmarkrao@gmail.com>
2019-09-21 00:43:05 +0200

    form with prev question jump, deps

--- testrunner/test.js
+++ testrunner/test.js
@@ -1,6 +1,6 @@
 require('chai').should()
 const sender = require('./sender.js')
-const {makeMocks, makeEcho, makePostback, makeTextResponse, makeReferral, makeSynthetic, getFields} = require('@vlab-research/mox')
+const {makeMocks, makeEcho, makeQR, makePostback, makeTextResponse, makeReferral, makeSynthetic, getFields} = require('@vlab-research/mox')
 const uuid = require('uuid');
 const zmq = require('zeromq')
 const sock = zmq.socket('sub');
@@ -27,35 +27,6 @@ describe('Test Bot flow Survey Integration Testing', () => {
     console.log('Test finished!');
   });
 
-  it('Waits for external event and continues after event',  (done) => {
-    bindedDone = done.bind(this)
-    const fields = getFields('forms/Ep5wnS.json')
-
-    testFlow = [
-      [fields[0], [makePostback(fields[0], userId, 0)]],
-      [fields[1], [makeSynthetic(userId, { type: 'external', value: {type: 'moviehouse:play', id: 164118668 }})]],
-      [fields[2], [makePostback(fields[2], userId, 0)]],
-      [fields[3], []]
-    ]
-
-    sender(makeReferral(userId, 'Ep5wnS'))
-
-  }).timeout(20000);
-
-  it('Waits for a timeout event and continues after event',  (done) => {
-    bindedDone = done.bind(this)
-    const fields = getFields('forms/Ep5wnS.json')
-
-    testFlow = [
-      [fields[0], [makePostback(fields[0], userId, 0)]],
-      [fields[1], []],
-      [fields[2], [makePostback(fields[2], userId, 0)]],
-      [fields[3], []]
-    ]
-
-    sender(makeReferral(userId, 'Ep5wnS'))
-
-  }).timeout(180000);
 
   it('Test chat flow with logic jump "Yes"',  (done) => {
     bindedDone = done.bind(this)
@@ -87,6 +58,55 @@ describe('Test Bot flow Survey Integration Testing', () => {
     sender(makeReferral(userId, 'LDfNCy'));
 
   }).timeout(20000);
+
+
+  it('Test chat flow with logic jump from previous question',  (done) => {
+    bindedDone = done.bind(this)
+    const fields = getFields('forms/jISElk.json')
+
+    testFlow = [
+      [fields[0], [makeQR(fields[0], userId, 1)]],
+      [fields[1], [makeQR(fields[1], userId, 5)]],
+      [fields[2], [makeTextResponse(userId, 'LOL')]],
+      [fields[4], []],
+      [fields[5], []],
+    ]
+
+    sender(makeReferral(userId, 'jISElk'))
+
+  }).timeout(20000);
+
+
+  it('Waits for external event and continues after event',  (done) => {
+    bindedDone = done.bind(this)
+    const fields = getFields('forms/Ep5wnS.json')
+
+    testFlow = [
+      [fields[0], [makePostback(fields[0], userId, 0)]],
+      [fields[1], [makeSynthetic(userId, { type: 'external', value: {type: 'moviehouse:play', id: 164118668 }})]],
+      [fields[2], [makePostback(fields[2], userId, 0)]],
+      [fields[3], []]
+    ]
+
+    sender(makeReferral(userId, 'Ep5wnS'))
+
+  }).timeout(20000);
+
+
+  it('Waits for a timeout event and continues after event',  (done) => {
+    bindedDone = done.bind(this)
+    const fields = getFields('forms/vHXzrh.json')
+
+    testFlow = [
+      [fields[0], []],
+      [fields[1], [makeTextResponse(userId, 'LOL')]],
+      [fields[2], []],
+    ]
+
+    sender(makeReferral(userId, 'vHXzrh'))
+
+  }).timeout(180000);
+
 
   sock.on('message', async (_, message) => {
     const msg = JSON.parse(message.toString()).message
