2b9ddea509bb364bf5e5b3f905583c42667cb44a
Nandan <nandanmarkrao@gmail.com>
2019-10-20 19:53:25 +0200

    added initial value to histogram reduction

--- src/containers/AnswersReport/chartUtil.js
+++ src/containers/AnswersReport/chartUtil.js
@@ -2,8 +2,8 @@ import {bin} from 'd3-array';
 
 export const computeHistogramData = (resultSet, interval, key) => {
   const d = resultSet.rawData().map(r => r[key]);
-  const max = d.reduce((a,b) => Math.max(a,b));
-  const min = d.reduce((a,b) => Math.min(a,b));
+  const max = d.reduce((a,b) => Math.max(a,b), 5);
+  const min = d.reduce((a,b) => Math.min(a,b), 0);
 
   const b = bin().thresholds(Math.ceil((max - min) / interval));
 
