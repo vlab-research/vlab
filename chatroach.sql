b7cd1394b25cd3426b17aa0334db71fb037dc883
Nandan <nandanmarkrao@gmail.com>
2020-02-09 02:48:23 +0100

    chatroach sql changes: shortcodes, facebook_pages

--- chatroach.sql
+++ chatroach.sql
@@ -14,37 +14,47 @@ CREATE TABLE chatroach.messages(
        timestamp TIMESTAMPTZ NOT NULL
 );
 
-
 CREATE TABLE chatroach.users(
-       id VARCHAR NOT NULL PRIMARY KEY,
+       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
        token VARCHAR NOT NULL,
        email VARCHAR NOT NULL UNIQUE
 );
 
+CREATE TABLE chatroach.facebook_pages(
+       pageid VARCHAR PRIMARY KEY,
+       userid UUID REFERENCES chatroach.users(id) ON DELETE CASCADE
+);
+
 CREATE TABLE chatroach.surveys(
-       id VARCHAR NOT NULL PRIMARY KEY,
+       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+       created TIMESTAMPTZ NOT NULL,
        formid VARCHAR NOT NULL,
        form VARCHAR NOT NULL,
        messages VARCHAR,
-       shortcode INT NOT NULL,
+       shortcode VARCHAR NOT NULL,
        title VARCHAR NOT NULL,
-       userid VARCHAR NOT NULL REFERENCES chatroach.users(email) ON DELETE CASCADE
+       userid UUID NOT NULL REFERENCES chatroach.users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE chatroach.responses(
-       formid VARCHAR NOT NULL REFERENCES chatroach.surveys(id),
+       parent_surveyid UUID NOT NULL REFERENCES chatroach.surveys(id),
+       parent_shortcode VARCHAR NOT NULL, -- implicit reference to surveys.shortcode
+       surveyid UUID NOT NULL REFERENCES chatroach.surveys(id),
+       shortcode VARCHAR NOT NULL, -- implicit reference to surveys.shortcode
        flowid INT NOT NULL,
        userid VARCHAR NOT NULL,
        question_ref VARCHAR NOT NULL,
        question_idx INT NOT NULL,
        question_text VARCHAR NOT NULL,
        response VARCHAR NOT NULL,
+       seed INT NOT NULL,
        timestamp TIMESTAMPTZ NOT NULL,
-       PRIMARY KEY (userid, timestamp)
+       PRIMARY KEY (userid, timestamp, question_ref) -- bit hacky, remove timestamp?
 );
 
 CREATE TABLE chatroach.timeouts(
        userid VARCHAR NOT NULL,
+       pageid VARCHAR NOT NULL REFERENCES chatroach.facebook_pages(pageid),
        timeout_date TIMESTAMPTZ,
        fulfilled BOOLEAN,
        PRIMARY KEY (userid, timeout_date)
@@ -57,3 +67,4 @@ GRANT INSERT,SELECT ON TABLE chatroach.responses to chatroach;
 GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.timeouts to chatroach;
 GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.users to chatroach;
 GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.surveys to chatroach;
+GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.facebook_pages to chatroach;
