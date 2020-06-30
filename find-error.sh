471e6853d0ed1cb22349b24d7324871110913cbe
Nandan Rao <nandanmarkrao@gmail.com>
2020-06-30 12:23:36 +0200

    Added namespace to find-error script

--- find-error.sh
+++ find-error.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-NAME=$1
-SEARCH=$2
+NS=$1
+NAME=$2
+SEARCH=$3
 
 # get number of pods
-NUM=$(kubectl get pods -l "app.kubernetes.io/name=${NAME}" | wc -l)
+NUM=$(kubectl -n $NS get pods -l "app.kubernetes.io/name=${NAME}" | wc -l)
 NUM=$(expr $NUM - 2)
 
 for i in $(eval echo {0..$NUM})
 do
-  kubectl logs $(kubectl get pods -l "app.kubernetes.io/name=${NAME}" -o jsonpath="{.items[${i}].metadata.name}") | grep "$SEARCH" -B 15 -A 10
+  kubectl -n $NS logs $(kubectl -n $NS get pods -l "app.kubernetes.io/name=${NAME}" -o jsonpath="{.items[${i}].metadata.name}") | grep "$SEARCH" -B 50 -A 50
 done
