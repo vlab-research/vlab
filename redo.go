2c212bab73f36afff2dfb46d84301978db8698f9
Nandan Rao <nandanmarkrao@gmail.com>
2020-08-28 00:57:43 +0200

    added blocked support with codes from env

--- redo.go
+++ redo.go
@@ -4,13 +4,40 @@ import (
     "context"
     "fmt"
     "log"
-    "github.com/jackc/pgx/v4"
+	"strings"
+    "github.com/jackc/pgx/v4/pgxpool"
 	"github.com/caarlos0/env/v6"
+	"sync"
 	"encoding/json"
 	"net/http"
 	"bytes"
 )
 
+func merge(cs ...<-chan *ExternalEvent) <-chan *ExternalEvent {
+    var wg sync.WaitGroup
+    out := make(chan *ExternalEvent)
+
+    // Start an output goroutine for each input channel in cs.  output
+    // copies values from c to out until c is closed, then calls wg.Done.
+    output := func(c <-chan *ExternalEvent) {
+        for n := range c {
+            out <- n
+        }
+        wg.Done()
+    }
+    wg.Add(len(cs))
+    for _, c := range cs {
+        go output(c)
+    }
+
+    // Start a goroutine to close out once all the output goroutines are
+    // done.  This must start after the wg.Add call.
+    go func() {
+        wg.Wait()
+        close(out)
+    }()
+    return out
+}
 
 func handle(err error) {
 	if err != nil {
@@ -25,6 +52,11 @@ type Config struct {
 	Host string `env:"CHATBASE_HOST,required"`
 	Port string `env:"CHATBASE_PORT,required"`
 	Botserver string `env:"BOTSERVER_URL,required"`
+	Codes string `env:"REDO_FB_CODES,required"`
+}
+
+func redoCodes(cfg *Config) []string {
+	return strings.Split(cfg.Codes, ",")
 }
 
 type Event struct {
@@ -37,14 +69,8 @@ type ExternalEvent struct {
 	Event *Event `json:"event"`
 }
 
-func get(conn *pgx.Conn, ch chan *ExternalEvent) {
-	query := `SELECT userid, pageid
-              FROM states
-              WHERE current_state = 'RESPONDING'
-              AND updated > now() - interval '23 hour'
-              AND now() - updated > interval '1 hour'`
-
-    rows, err := conn.Query(context.Background(), query)
+func get(conn *pgxpool.Pool, ch chan *ExternalEvent, query string, args ...interface{}) {
+    rows, err := conn.Query(context.Background(), query, args...)
 	handle(err)
 
 	go func () {
@@ -59,6 +85,29 @@ func get(conn *pgx.Conn, ch chan *ExternalEvent) {
 		}
 	}()
 }
+
+func respondings(conn *pgxpool.Pool) chan *ExternalEvent {
+	ch := make(chan *ExternalEvent)
+	query := `SELECT userid, pageid
+              FROM states
+              WHERE current_state = 'RESPONDING'
+              AND updated > now() - interval '23 hour'
+              AND now() - updated > interval '1 hour'`
+
+	get(conn, ch, query)
+	return ch
+}
+
+func blocked(cfg *Config, conn *pgxpool.Pool) chan *ExternalEvent {
+	ch := make(chan *ExternalEvent)
+	query := `SELECT userid, pageid 
+              FROM states 
+              WHERE state_json->'error'->>'code' = ANY($1)`
+
+	get(conn, ch, query, redoCodes(cfg))
+	return ch
+}
+
 
 func send(cfg *Config, client *http.Client, e *ExternalEvent) error {
 
@@ -83,7 +132,7 @@ func send(cfg *Config, client *http.Client, e *ExternalEvent) error {
 }
 
 
-func process(cfg *Config, ch chan *ExternalEvent) {
+func process(cfg *Config, ch <-chan *ExternalEvent) {
 	client := &http.Client{}
 
 	counter := 0
@@ -95,16 +144,16 @@ func process(cfg *Config, ch chan *ExternalEvent) {
 	log.Printf("Successfully sent %v new redo events", counter)
 }
 
-func getConn(cfg *Config) (*pgx.Conn, context.Context) {
+func getConn(cfg *Config) *pgxpool.Pool {
 	conString := fmt.Sprintf("postgresql://%s@%s:%s/%s?sslmode=disable", cfg.User, cfg.Host, cfg.Port, cfg.Db)
-    config, err := pgx.ParseConfig(conString)
+    config, err := pgxpool.ParseConfig(conString)
 	handle(err)
 
 	ctx := context.Background()
-    conn, err := pgx.ConnectConfig(ctx, config)
+    pool, err := pgxpool.ConnectConfig(ctx, config)
 	handle(err)
 
-	return conn, ctx
+	return pool
 }
 
 func main() {
@@ -112,10 +161,9 @@ func main() {
 	err := env.Parse(&cfg)
 	handle(err)
 
-	conn, ctx := getConn(&cfg)
-	defer conn.Close(ctx)
-
-	ch := make(chan *ExternalEvent)
-	go get(conn, ch)
+	conn := getConn(&cfg)
+	defer conn.Close()
+	
+	ch := merge(respondings(conn), blocked(&cfg, conn))
 	process(&cfg, ch)
 }
