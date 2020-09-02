dadf14b4beff3f5df12ab2878f158eb33353e5d9
Nandan Rao <nandanmarkrao@gmail.com>
2020-09-02 16:18:08 +0200

    added hacky timeout to receiver, sends error, cleanup kube files

--- receiver/dev.sh
+++ receiver/dev.sh
@@ -1,17 +1,6 @@
-eval $(minikube docker-env)
+kubectl delete -f ../kube/facebot.yaml
+docker build -t localhost:5000/facebot:registry .
+docker push localhost:5000/facebot:registry
+kubectl apply -f ../kube/facebot.yaml
 
-# Secrets
-#kubectl delete secret gbv-bot-envs
-#kubectl create secret generic gbv-bot-envs --from-env-file .env
 
-# App
-kubectl delete -f ../kube
-
-docker build -t localhost:32000/facebot:registry .
-docker push localhost:32000/facebot:registry
-
-kubectl apply -f ../kube/deployment.yaml
-kubectl apply -f ../kube/broker.yaml
-kubectl apply -f ../kube/service.yaml
-
-# kubelog gbv-facebot 0 --follow
--- receiver/index.js
+++ receiver/index.js
@@ -16,11 +16,19 @@ app.get('/:id', (req, res) => res.send(getUser(req.params.id)))
 
 
 app.post('/me/messages', express.json(), async (req, res) => {
-  req.setTimeout(5000)
+  let sent;
+  
+  setTimeout(() => {
+    if (sent) return
+    res.json({error: {message: 'Facebot has no answer to give.', code: 99999 }})
+    sent = true
+  }, 15000)
 
   sock.once('message', (message) => {
+    if (sent) return
     const response = JSON.parse(message.toString())
     res.send(response)
+    sent = true
   })
 
   const data = JSON.stringify(req.body)
