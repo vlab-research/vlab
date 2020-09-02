02e8d7f706804ccd15ff5ddfec5f29f5ca403bad
Nandan Rao <nandanmarkrao@gmail.com>
2020-09-02 23:22:04 +0200

    changed migrate-4, timeout_date only for timeout waits

--- sql/migrate-4.sql
+++ sql/migrate-4.sql
@@ -2,11 +2,12 @@
 
 -- Add created columns
 ALTER TABLE chatroach.states ADD COLUMN fb_error_code varchar AS (state_json->'error'->>'code') STORED;
-ALTER TABLE chatroach.states ADD COLUMN timeout_date TIMESTAMPTZ AS (CEILING((state_json->>'waitStart')::INT/1000)::INT::TIMESTAMPTZ + (state_json->'wait'->>'value')::INTERVAL) STORED;
+ALTER TABLE chatroach.states ADD COLUMN timeout_date TIMESTAMPTZ AS (CASE
+      WHEN state_json->'wait'->>'type' = 'timeout' THEN (CEILING((state_json->>'waitStart')::INT/1000)::INT::TIMESTAMPTZ + (state_json->'wait'->>'value')::INTERVAL)
+      ELSE NULL
+END) STORED;
+
 
 -- Index those columns for queries!
 CREATE INDEX ON chatroach.states (current_state, fb_error_code) STORING (state_json);
 CREATE INDEX ON chatroach.states (current_state, timeout_date) STORING (state_json);
-
-
-
