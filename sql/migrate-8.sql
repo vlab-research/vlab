d8b08c5a1a7b4c7f9efc9c3ab421cf6358b3b9f8
Nandan Rao <nandanmarkrao@gmail.com>
2020-09-16 13:51:31 +0200

    fixed bug in migrate-8, pageid in surveys

--- sql/migrate-8.sql
+++ sql/migrate-8.sql
@@ -6,7 +6,7 @@ ALTER TABLE chatroach.states ADD COLUMN current_form varchar AS (state_json->'fo
 CREATE INDEX ON chatroach.states (current_state, current_form, updated);
 
 ALTER TABLE chatroach.surveys ADD COLUMN form_json JSON AS (form::JSON) STORED;
-CREATE INDEX ON chatroach.surveys (shortcode, userid, pageid, created DESC) STORING (formid, form, messages, title, form_json);
+CREATE INDEX ON chatroach.surveys (shortcode, userid, created DESC) STORING (formid, form, messages, title, form_json);
 
 ALTER TABLE chatroach.surveys ADD COLUMN messages_json JSON AS (messages::JSON) STORED;
 ALTER TABLE chatroach.surveys ADD COLUMN has_followup BOOL AS (messages::JSON->>'label.buttonHint.default' IS NOT NULL) STORED;
