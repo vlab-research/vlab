c08ef96ede58a819355fc69f487e0ea35461fb4e
Nandan Rao <nandanmarkrao@gmail.com>
2020-12-01 13:55:11 +0100

    new versions with form central, migrate 14 with not null

--- sql/migrate-14.sql
+++ sql/migrate-14.sql
@@ -1,5 +1,5 @@
 ALTER TABLE chatroach.responses ADD COLUMN translated_response VARCHAR;
 
-ALTER TABLE chatroach.surveys ADD COLUMN metadata JSONB DEFAULT '{}';
-ALTER TABLE chatroach.surveys ADD COLUMN survey_name VARCHAR DEFAULT 'default';
-ALTER TABLE chatroach.surveys ADD COLUMN translation_conf JSONB DEFAULT '{}';
+ALTER TABLE chatroach.surveys ADD COLUMN metadata JSONB NOT NULL DEFAULT '{}';
+ALTER TABLE chatroach.surveys ADD COLUMN survey_name VARCHAR NOT NULL DEFAULT 'default';
+ALTER TABLE chatroach.surveys ADD COLUMN translation_conf JSONB NOT NULL DEFAULT '{}';
