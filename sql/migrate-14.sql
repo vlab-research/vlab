5057c4ae910a833cc71f436f7762df1fc7c2934f
Nandan Rao <nandanmarkrao@gmail.com>
2020-12-01 10:23:52 +0100

    removed migrate-16

--- sql/migrate-14.sql
+++ sql/migrate-14.sql
@@ -3,5 +3,3 @@ ALTER TABLE chatroach.responses ADD COLUMN translated_response VARCHAR;
 ALTER TABLE chatroach.surveys ADD COLUMN metadata JSONB DEFAULT '{}';
 ALTER TABLE chatroach.surveys ADD COLUMN survey_name VARCHAR DEFAULT 'default';
 ALTER TABLE chatroach.surveys ADD COLUMN translation_conf JSONB DEFAULT '{}';
-
-
