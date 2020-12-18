4fb109aa115286a2a6f49a574e3d5dd2871a21f1
Nandan Rao <nandanmarkrao@gmail.com>
2020-12-18 22:26:52 +0100

    removed states permission assignment in base chatroach.sql

--- sql/chatroach.sql
+++ sql/chatroach.sql
@@ -74,7 +74,6 @@ GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.timeouts to chatroach;
 GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.users to chatroach;
 GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.surveys to chatroach;
 GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.facebook_pages to chatroach;
-GRANT INSERT,SELECT,UPDATE ON TABLE chatroach.states to chatroach;
 
 CREATE USER chatreader;
 GRANT SELECT ON TABLE chatroach.messages to chatreader;
