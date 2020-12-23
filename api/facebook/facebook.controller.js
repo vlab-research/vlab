d1a97afc90b9195b3a42c479d9ef6390cbcd2fd8
Nandan Rao <nandanmarkrao@gmail.com>
2020-12-23 02:15:13 +0100

    added facebook webhooks post route

--- api/facebook/facebook.controller.js
+++ api/facebook/facebook.controller.js
@@ -12,8 +12,34 @@ exports.exchangeToken = async (req, res) => {
   if (r.error) {
     // TODO: put into general error handling (next)
     console.error(r.error);
-    return res.status(500).json(r.error);
+    return res.status(400).json(r.error);
   }
 
   return res.json({ access_token: r.access_token });
+}
+
+exports.addWebhooks = async (req, res) => {
+  const {pageid, token} = req.body;
+
+  const json = {
+    subscribed_fields: ['messages',
+                        'messaging_postbacks',
+                        'messaging_optins',
+                        'messaging_account_linking',
+                        'messaging_referrals',
+                        'message_echoes',
+                        'messaging_handovers',
+                        'messaging_policy_enforcement']
+  }
+
+  const url = `https://graph.facebook.com/v9.0/${pageid}/subscribed_apps?access_token=${token}`
+  const r = await r2.post(url, { json }).json
+
+  if (r.error) {
+    // TODO: put into general error handling (next)
+    console.error(r.error);
+    return res.status(400).json(r.error);
+  }
+
+  return res.status(201).json(r);
 }
--- api/facebook/facebook.routes.js
+++ api/facebook/facebook.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('./facebook.controller');
 
 router.post('/exchange-token', controller.exchangeToken);
+router.post('/webhooks', controller.addWebhooks);
 
 module.exports = router;
