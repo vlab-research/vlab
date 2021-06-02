5a8bed387626e4fb486f6f12a16c3c6616a39810
ericrfdev <ericrfdev@hotmail.com>
2021-06-02 23:16:16 +0200

    (#5) Setup the frontend codebase for the dashboard   - Install React with Typescript through create-react-app   - Install React Router DOM   - Install Tailwind, TailwindUI and it's required dependencies (PostCSS, autoprefixer, @headlessui/react, @heroicons/react)   - Setup Inter font as default one. (TailwindUI components have been created using this font)   - Use CRACO to override the default PostCSS config from create-react-app. This is needed because Tailwind it's a PostCSS plugin and needs to be listed in it's config file   - Lock the node version to the current LTS (14.17.0)   - Install prettier and setup a minimum conf. I decided to not setup a precommit hook at this stage   - Add Netlify config file

---
+++ dashboard-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-200 min-h-screen p-8 flex items-center justify-center antialiased">
+      <span className="font-bold text-2xl text-gray-900">Welcome!</span>
+    </div>
+  );
+}
+
+export default App;
