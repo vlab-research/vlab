bfe0cc03fc04be6d88fd333e260a7b0686e68369
Nandan Rao <nandanmarkrao@gmail.com>
2021-06-30 21:04:17 +0200

    replybot: simplified dev.sh, removed old minikube logic

--- replybot/dev.sh
+++ replybot/dev.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-# eval $(minikube docker-env)
-
-# Secrets
-# kubectl delete secret gbv-replybot-keys
-# kubectl delete secret gbv-bot-envs
-# kubectl create secret generic gbv-replybot-keys --from-file=keys
-# kubectl create secret generic gbv-bot-envs --from-env-file .env
-
 # App
 kubectl delete -f kube-dev
-# kubectl delete -f kube-dev/scratch-deployment.yaml
 docker build -t localhost:5000/replybot:registry .
 docker push localhost:5000/replybot:registry
 kubectl apply -f kube-dev
-# kubectl delete -f kube-dev/scratch-deployment.yaml
 
 
