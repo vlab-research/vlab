cf917b08cb93a77435da81ed4e7423ac3562864c
Carlos Chinchilla <calufa@gmail.com>
2021-09-23 19:41:30 -0600

    fix style

--- dinersclub/reloadly_test.go
+++ dinersclub/reloadly_test.go
@@ -11,23 +11,21 @@ import (
 )
 
 func TestReloadlyResultsOnErrorIfBadDetails(t *testing.T) {
-	provider := &ReloadlyProvider{&ReloadlyConfig{}, &reloadly.Service{
-		Client: &http.Client{},
-	}}
-
-	jm := json.RawMessage([]byte(`{"foo": "bar"}`))
-
 	ts := JSTimestamp(time.Now().UTC())
-
+	details := json.RawMessage([]byte(`{"foo": "bar"}`))
 	pe := &PaymentEvent{
 		Userid:    "foo",
 		Pageid:    "page",
 		Timestamp: &ts,
 		Provider:  "reloadly",
-		Details:   &jm,
+		Details:   &details,
 	}
-
+	service := &reloadly.Service{
+		Client: &http.Client{},
+	}
+	provider := &ReloadlyProvider{&ReloadlyConfig{}, service}
 	res, err := provider.Payout(pe)
+
 	assert.Nil(t, err)
 	assert.NotNil(t, res.Error)
 	assert.Equal(t, "INVALID_PAYMENT_DETAILS", res.Error.Code)
@@ -36,50 +34,50 @@ func TestReloadlyResultsOnErrorIfBadDetails(t *testing.T) {
 }
 
 func TestReloadlyReportsAPIErrorsInResult(t *testing.T) {
-	provider := &ReloadlyProvider{&ReloadlyConfig{}, &reloadly.Service{
-		Client: TestClient(404, `{"errorCode": "FOOBAR", "message": "Sorry"}`, nil),
-	}}
-
-	jm := json.RawMessage([]byte(`{"number": "+123", "amount": 2.5, "country": "IN", "id": "id"}`))
 	ts := JSTimestamp(time.Now().UTC())
+	details := json.RawMessage([]byte(`{"number": "+123", "amount": 2.5, "country": "IN", "id": "id"}`))
 	pe := &PaymentEvent{
 		Userid:    "foo",
 		Pageid:    "page",
 		Timestamp: &ts,
 		Provider:  "reloadly",
-		Details:   &jm,
+		Details:   &details,
 	}
-
+	service := &reloadly.Service{
+		Client: TestClient(404, `{"errorCode": "FOOBAR", "message": "Sorry"}`, nil),
+	}
+	provider := &ReloadlyProvider{&ReloadlyConfig{}, service}
 	res, err := provider.Payout(pe)
+
 	assert.Nil(t, err)
 	assert.NotNil(t, res.Error)
 	assert.Equal(t, "FOOBAR", res.Error.Code)
 	assert.Equal(t, "Sorry", res.Error.Message)
-	assert.Equal(t, &jm, res.Error.PaymentDetails)
+	assert.Equal(t, &details, res.Error.PaymentDetails)
 	assert.Equal(t, "id", res.ID)
 	assert.Equal(t, "payment:reloadly", res.Type)
 	assert.Equal(t, false, res.Success)
 }
 
 func TestReloadlyReportsSuccessResult(t *testing.T) {
-	provider := &ReloadlyProvider{&ReloadlyConfig{}, &reloadly.Service{
-		Client: TestClient(200, `{"suggestedAmountsMap":{"2.5": 2.5},"transactionDate":"2020-09-19 12:53:22","transactionId": 567}`, nil),
-	}}
-
-	jm := json.RawMessage([]byte(`{"number": "+123", "amount": 2.5, "country": "IN"}`))
 	ts := JSTimestamp(time.Now().UTC())
+	details := json.RawMessage([]byte(`{"number": "+123", "amount": 2.5, "country": "IN"}`))
 	pe := &PaymentEvent{
 		Userid:    "foo",
 		Pageid:    "page",
 		Timestamp: &ts,
 		Provider:  "reloadly",
-		Details:   &jm,
+		Details:   &details,
 	}
-
+	service := &reloadly.Service{
+		Client: TestClient(200, `{"suggestedAmountsMap":{"2.5": 2.5},"transactionDate":"2020-09-19 12:53:22","transactionId": 567}`, nil),
+	}
+	provider := &ReloadlyProvider{&ReloadlyConfig{}, service}
 	res, err := provider.Payout(pe)
+
 	assert.Nil(t, err)
 	assert.Nil(t, res.Error)
 	assert.Equal(t, "payment:reloadly", res.Type)
 	assert.Equal(t, true, res.Success)
-	assert.Equal(t, &jm, res.PaymentDetails)
+	assert.Equal(t, &details, res.PaymentDetails)
 }
