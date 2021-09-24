e59d35fb063f0ee216310f7f7e842434b1c1cbef
Carlos Chinchilla <calufa@gmail.com>
2021-09-23 19:41:53 -0600

    remove dead files

--- dinersclub/dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# App
-helm delete dinersclub
-docker build -t localhost:5000/dinersclub:registry .
-docker push localhost:5000/dinersclub:registry
-helm upgrade --install dinersclub chart/ -f values.yaml
