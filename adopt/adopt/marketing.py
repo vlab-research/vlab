a4620868ca8f30b93e2f4dfa3f2dce06523356b1
Nandan Rao <nandanmarkrao@gmail.com>
2021-11-17 04:59:03 +0100

    adopt: removed approximate_audience which has been deprecated

--- adopt/adopt/marketing.py
+++ adopt/adopt/marketing.py
@@ -236,7 +236,7 @@ def manage_aud(old_auds: List[CustomAudience], aud: Audience) -> List[Instructio
     instructions = add_users_to_audience(aud.pageid, ca.get_id(), aud.users)
 
     if aud.subtype == "LOOKALIKE" and aud.lookalike is not None:
-        count = ca["approximate_count"]
+        count = len(aud.users)
 
         # use aud.users as size? This would work if Facebook is immediate about
         # counting the new users, otherwise it will fail.
