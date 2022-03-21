470aaf14501fcc9ad87f540482554e9d7e5a108e
Nandan Rao <nandanmarkrao@gmail.com>
2022-03-21 01:24:41 +0100

    undid the stupid db conn fixture, wasn't needed

--- adopt/test/dbfix.py
+++ adopt/test/dbfix.py
@@ -1,42 +1,15 @@
-from dataclasses import dataclass
-
 import psycopg
 import pytest
 
-from adopt.db import _query
+from adopt.db import _connect
 
 cnf = "postgresql://root@localhost:5433/test"
 
 
-@dataclass
-class DB:
-    conn: psycopg.Connection
-
-    def query(self, q, vals=(), as_dict=False):
-        for x in _query(self.conn.cursor(), q, vals, as_dict):
-            yield x
-        self.conn.commit()
-
-    def execute(self, q, vals=()):
-        with self.conn.cursor() as cur:
-            cur.execute(q, vals)
-            self.conn.commit()
-
-    def reset(self):
-        with self.conn.cursor() as cur:
-            tables = ["study_confs", "recruitment_data_events", "studies", "users"]
+def _reset_db():
+    with _connect(cnf) as conn:
+        with conn.cursor() as cur:
+            tables = ["recruitment_data_events", "study_confs", "studies", "users"]
             for t in tables:
-                cur.execute(f"delete from {t} cascade")
-            self.conn.commit()
-
-
-@pytest.fixture(scope="package")
-def db():
-    conn = psycopg.connect(cnf)
-    db = DB(conn)
-
-    db.reset()
-    yield db
-
-    # teardown
-    db.conn.close()
+                cur.execute(f"delete from {t}")
+                conn.commit()
