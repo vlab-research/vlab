aba758b85fe6ff1fb077aed0a476fbc12ab7045b
Nandan Rao <nandanmarkrao@gmail.com>
2022-03-22 00:36:06 +0100

    view for active based on recruitment

---
+++ devops/test.sh
@@ -0,0 +1,12 @@
+function cleanup {
+  ./stop.sh
+}
+
+trap cleanup EXIT
+cleanup
+
+echo "\n>>> SETTING UP API <<<\n"
+docker-compose -f docker-compose-common.yaml -f docker-compose-test.yaml build api
+
+echo "\n>>> RUNNING TEST CASES <<<\n"
+docker-compose -f docker-compose-common.yaml -f docker-compose-test.yaml run api
