aa8cc1182abd204eac4e7152987eba71ac753da0
Sophie <sophie@sssoja.com>
2022-07-15 17:01:31 +0200

    add placeholders to inputs

--- dashboard-frontend/src/pages/ConnectPage/InputSecret.tsx
+++ dashboard-frontend/src/pages/ConnectPage/InputSecret.tsx
@@ -1,21 +1,23 @@
 const InputSecret = () => (
   <>
-    <div className="flex flex-col mx-4 my-4 sm:my-0">
+    <div className="flex flex-col mb-4">
       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
         Enter client ID
       </label>
       <input
         type="text"
         className="bg-gray-100 rounded p-2 border focus:outline-none focus:border-blue-500"
+        placeholder="12345"
       ></input>
     </div>
-    <div className="flex flex-col mx-4 my-4 sm:my-0">
+    <div className="flex flex-col">
       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
         Enter client secret
       </label>
       <input
         type="text"
         className="bg-gray-100 rounded p-2 border focus:outline-none focus:border-blue-500"
+        placeholder="qwertyuiopsdfghjcvbn"
       ></input>
     </div>
   </>
--- dashboard-frontend/src/pages/ConnectPage/InputToken.tsx
+++ dashboard-frontend/src/pages/ConnectPage/InputToken.tsx
@@ -1,12 +1,13 @@
 const InputToken = () => (
   <>
-    <div className="flex flex-col mx-4 my-4 sm:my-0">
+    <div className="flex flex-col sm:mb-4">
       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
         Enter access token
       </label>
       <input
         type="text"
         className="bg-gray-100 rounded p-2 border focus:outline-none focus:border-blue-500"
+        placeholder="qwertyuiopsdfghjcvbn"
       ></input>
     </div>
   </>
