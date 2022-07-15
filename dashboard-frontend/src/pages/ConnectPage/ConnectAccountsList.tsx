d894c9dfac295f2c158599008b917af3ebf4a32c
Sophie <sophie@sssoja.com>
2022-07-15 15:35:59 +0200

    use grid layout

--- dashboard-frontend/src/pages/ConnectPage/ConnectAccountsList.tsx
+++ dashboard-frontend/src/pages/ConnectPage/ConnectAccountsList.tsx
@@ -27,13 +27,16 @@ const AccountListItem = ({
 }) => (
   <li>
     <div className="px-4 py-4 sm:px-6">
-      <div className="flex flex-col sm:flex-row sm:items-center">
-        <p className="text-sm font-medium text-indigo-600 truncate">{name}</p>
-
-        {authType === 'secret' ? <InputSecret /> : <InputToken />}
+      <div className="flex flex-col sm:grid grid-cols-5 gap-4">
+        <p className="text-sm font-medium text-indigo-600 truncate col-span-1">
+          {name}
+        </p>
+        <div className="col-span-2">
+          {authType === 'secret' ? <InputSecret /> : <InputToken />}
+        </div>
         <button
           className={classNames(
-            'sm:self-end items-center px-4 py-2 mx-4 my-4 sm:my-0 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
+            'sm:self-end items-center px-4 py-2 mx-4 my-4 sm:my-0 col-span-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
           )}
         >
           <Link to={`/connect/${slug}`} className="block hover:bg-gray-50">
