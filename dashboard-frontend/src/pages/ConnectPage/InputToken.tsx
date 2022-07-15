ee36ace1824fb35d7da7e85d629d46bc7330c3b5
Sophie <sophie@sssoja.com>
2022-07-15 14:46:51 +0200

    create separate components for token and client secret

---
+++ dashboard-frontend/src/pages/ConnectPage/InputToken.tsx
@@ -0,0 +1,15 @@
+const InputToken = () => (
+  <>
+    <div className="flex flex-col mx-4 my-4 sm:my-0">
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+        Enter access token
+      </label>
+      <input
+        type="text"
+        className="bg-gray-100 rounded p-2 border focus:outline-none focus:border-blue-500"
+      ></input>
+    </div>
+  </>
+);
+
+export default InputToken;
