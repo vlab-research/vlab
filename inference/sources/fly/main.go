84224ab8cd02514437b4958e87e2a19c52c9638d
juancarrillodev <juancarrillodev@gmail.com>
2022-07-21 21:10:35 -0600

    comets

--- inference/sources/fly/main.go
+++ inference/sources/fly/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/dghubble/sling"
+	"github.com/vlab-research/vlab/inference/connector"
 	. "github.com/vlab-research/vlab/inference/inference-data"
 )
+
+func (c flyConnector) GetToken() string {
+	url := "https://dev-x7eacpbs.us.auth0.com/oauth/token"
+	devUrl := "https://dev-x7eacpbs.us.auth0.com/api/v2/"
+	payload := strings.NewReader("grant_type=client_credentials&client_id=" + c.ClientId + "&client_secret=" + c.ClientSecret + "&audience=" + devUrl)
+	req, _ := http.NewRequest("POST", url, payload)
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	res, _ := http.DefaultClient.Do(req)
+	// defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println("res ->", res)
+	// fmt.Println(string(body))
+	return string(body)
+}
 
 func handle(err error) {
 	if err != nil {
@@ -22,37 +41,38 @@ type GetResponsesParams struct {
 }
 
 type GetResponsesResponse struct {
-	TotalItems int `json:"total_items"`
-	PageCount  int `json:"page_count"`
-	Items      []struct {
-		Parent_surveyid  string `json:"parent_surveyid"`
-		Parent_shortcode string `json:"parent_shortcode"`
-		Surveyid         string `json:"surveyid"`
-		Flowid           string `json:"flowid"`
-		Userid           string `json:"userid"`
-		Question_ref     string `json:"question_ref"`
-		Question_idx     string `json:"question_idx"`
-		Question_text    string `json:"question_text"`
-		Response         struct {
-			Text string `json:"user_agent"`
-		} `json:"res"`
-		Timestamp string `json:"timestamp"`
-		Metadata  struct {
-			UserAgent string `json:"user_agent"`
-			Platform  string `json:"platform"`
-			Referer   string `json:"referer"`
-			NetworkID string `json:"network_id"`
-			Browser   string `json:"browser"`
-		} `json:"metadata"`
-		Pageid              string `json:"pageid"`
-		Translated_response string `json:"translated_response"`
+	Items []struct {
+		Parent_surveyid  string    `json:"parent_surveyid"`
+		Token            string    `json:"token"`
+		Parent_shortcode string    `json:"parent_shortcode"`
+		Surveyid         string    `json:"surveyid"`
+		Flowid           string    `json:"flowid"`
+		Userid           string    `json:"userid"`
+		Question_ref     string    `json:"question_ref"`
+		Question_idx     string    `json:"question_idx"`
+		Question_text    string    `json:"question_text"`
+		Response         string    `json:"response"`
+		Timestamp        time.Time `json:"timestamp"`
+		Metadata         struct {
+			Text string `json:"type"`
+		} `json:"Metadata"`
+		Pageid              string                     `json:"pageid"`
+		Translated_response string                     `json:"translated_response"`
+		Hidden              map[string]json.RawMessage `json:"hidden"`
 	} `json:"items"`
 }
 
+type Answer struct {
+	Response            string
+	Translated_response string
+}
+
 type flyConnector struct {
-	BaseUrl  string `env:"FLY_BASE_URL,required"`
-	Key      string `env:"FLY_KEY,required"`
-	PageSize int    `env:"FLY_PAGE_SIZE,required"`
+	BaseUrl      string `env:"FLY_BASE_URL,required"`
+	Key          string `env:"FLY_KEY,required"`
+	PageSize     int    `env:"FLY_PAGE_SIZE,required"`
+	ClientId     string `env:"FLY_CLIENT_ID,required"`
+	ClientSecret string `env:"FLY_CLIENT_SECRET,required"`
 }
 
 type FlyError struct {
@@ -96,9 +116,11 @@ func (c flyConnector) GetResponses(source *Source, form string, token string, id
 	events := make(chan *InferenceDataEvent)
 	params := &GetResponsesParams{PageSize: c.PageSize, After: token}
 	client := &http.Client{}
+
+	// GetTOKEN()
 	go func() {
 		defer close(events)
-		// for loop to paginate here?
+		// TODO: for loop to paginate here?
 		params.After = token
 		for {
 			res, err := Call(client, c.BaseUrl, c.Key, form, params)
@@ -106,21 +128,39 @@ func (c flyConnector) GetResponses(source *Source, form string, token string, id
 			if err != nil {
 				handle(err)
 			}
-
 			for _, item := range res.Items {
-				fmt.Printf("response: %v\n \n", item.Surveyid)
-				//Todo:  sabe database
-				params.After = item.Pageid
+				// for pagination
+				params.After = item.Token
+
+				md := item.Hidden
+
+				p := Answer{
+					Response:            item.Response,
+					Translated_response: item.Translated_response,
+				}
+
+				datosJson, err := json.Marshal(p)
+				if err != nil {
+					log.Fatal(err)
+				}
+
 				idx++
 				event := &InferenceDataEvent{
+					User:       User{ID: item.Userid, Metadata: md},
 					Study:      source.StudyID,
 					SourceConf: source.Conf,
+					Timestamp:  item.Timestamp,
 					Idx:        idx,
+					Pagination: item.Token,
+					// Here I am not sure if it is the equivalent
+					Variable: item.Question_ref,
+					Value:    []byte(datosJson),
 				}
 				events <- event
 			}
+			// }
 
-			if res.TotalItems == 2 {
+			if len(res.Items) < params.PageSize {
 				break
 			}
 
@@ -135,17 +175,7 @@ func (c flyConnector) Handler(source *Source, lastEvent *InferenceDataEvent) <-c
 }
 
 func main() {
-	// todo: temporarily  flyConnector
 	c := flyConnector{}
 	c.loadEnv()
-
-	cnf := &SourceConf{
-		Name:   "",
-		Source: "",
-		Config: []byte(`foo`),
-	}
-
-	events := c.GetResponses(&Source{"flys", cnf}, "formfoo", "oldtoken", 350)
-
-	fmt.Printf("Fin: %v\n", events)
+	connector.LoadEvents(c, "fly", "idx")
 }
--- inference/sources/fly/main_test.go
+++ inference/sources/fly/main_test.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"regexp"
-	"strings"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	. "github.com/vlab-research/vlab/inference/inference-data"
@@ -27,57 +27,83 @@ func resData(fi string) string {
 	return string(b)
 }
 
-func normalizeSpace(s string) string {
-	re := regexp.MustCompile(`\s+`)
-	r := re.Split(s, -1)
-	return strings.Join(r, " ")
-}
-
 func dataAssertions(t *testing.T, e []*InferenceDataEvent) {
-	assert.Equal(t, 2, len(e))
+	assert.Equal(t, 8, len(e))
 
-	for i := 0; i < len(e); i++ {
-		assert.Equal(t, 1, e[0].Idx)
-		assert.Equal(t, 2, e[1].Idx)
-	}
+	assert.Equal(t, "126", e[0].User.ID)
+	assert.Equal(t, "129", e[3].User.ID)
+	assert.Equal(t, "133", e[7].User.ID)
+
+	assert.Equal(t, "ref", e[0].Variable)
+	assert.Equal(t, "phone_number", e[3].Variable)
+	assert.Equal(t, "my_custom_dropdown_reference", e[7].Variable)
+
+	timestamp := time.Time(time.Date(2022, time.July, 21, 22, 33, 56, 0, time.UTC))
+	assert.Equal(t, timestamp, e[0].Timestamp)
+
+	timestamp = time.Time(time.Date(2017, time.September, 14, 22, 33, 56, 0, time.UTC))
+	assert.Equal(t, timestamp, e[3].Timestamp)
+
+	ans := `{"Response":"last","Translated_response":"último"}`
+	assert.Equal(t, ans, string(e[0].Value))
+	ans = `{"Response":"first","Translated_response":"primero"}`
+	ans = `{"Response":"hello","Translated_response":"hola"}`
+	assert.Equal(t, ans, string(e[7].Value))
 }
 
 func TestGetResponses_PaginatesWhenPageIsFull(t *testing.T) {
 	res1 := resData("fly_example.json")
-	res2 := resData("fly_example.json")
+	res2 := resData("fly_example2.json")
 
-	count := 1
+	s := string(res1)
+	data := GetResponsesResponse{}
+	json.Unmarshal([]byte(s), &data)
+
+	s2 := string(res2)
+	data2 := GetResponsesResponse{}
+	json.Unmarshal([]byte(s2), &data2)
+
+	// lastToken := data.Items[len(data.Items)-1]
+
+	count := 0
 	ts, _ := TestServer(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "/all", r.URL.Path)
+		after := r.URL.Query().Get("after")
 
 		if count == 0 {
-			assert.Equal(t, "after=oldtoken&page_size=3", r.URL.RawQuery)
-			w.WriteHeader(200)
+			assert.Condition(t, func() bool {
+				if after == "" {
+					return after != ""
+				}
+				return true
+			}, "No token sent")
+
+			assert.Equal(t, "oldtoken", after)
+			w.WriteHeader(http.StatusOK)
 			w.Header().Set("Content-Type", "application/json")
 			fmt.Fprint(w, res1)
 		}
 
 		if count == 1 {
-			assert.Equal(t, "after=be5ae9dd-0189-478e-8a3d-4d8ead8240a4&page_size=3", r.URL.RawQuery)
-
-			w.WriteHeader(200)
+			assert.Equal(t, "4viu4r8djwxwb2udbivx42avnawwj5wj", after)
+			w.WriteHeader(http.StatusOK)
 			w.Header().Set("Content-Type", "application/json")
 			fmt.Fprint(w, res2)
 		}
 
+		if count == 2 {
+			assert.Equal(t, "3btcnj9rrhzyttmghhapu6znz3y43i36", after)
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+		}
+
 		count++
+
 	})
 
-	tc := flyConnector{BaseUrl: ts.URL, Key: "sosecret", PageSize: 3}
+	tc := flyConnector{BaseUrl: ts.URL, Key: "sosecret", PageSize: 2}
 
-	cnf := &SourceConf{
-		Name:   "",
-		Source: "",
-		Config: []byte(`foo`),
-	}
-	study := "1"
-
-	events := tc.GetResponses(&Source{StudyID: study, Conf: cnf}, "formfoo", "be5ae9dd-0189-478e-8a3d-4d8ead8240a4", 0)
+	events := tc.GetResponses(&Source{StudyID: res1}, "formfoo", "oldtoken", 0)
 
 	e := Sliceit(events)
 	dataAssertions(t, e)
@@ -90,40 +116,46 @@ func TestGetResponses_AddsHiddenFieldsAsUserMetadata(t *testing.T) {
 	data := GetResponsesResponse{}
 	json.Unmarshal([]byte(s), &data)
 
-	count := 0
 	ts, _ := TestServer(func(w http.ResponseWriter, r *http.Request) {
-
 		assert.Equal(t, "/all", r.URL.Path)
 
-		if count == 0 {
-			assert.Equal(t, "other", data.Items[0].Metadata.Platform)
-
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			fmt.Fprint(w, res)
-		}
-
-		if count == 1 {
-			assert.Equal(t, "other", data.Items[1].Metadata.Platform)
-
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			fmt.Fprint(w, res)
-		}
-
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, res)
 	})
-	count++
 
 	tc := flyConnector{BaseUrl: ts.URL, Key: "sosecret", PageSize: 5}
 
-	events := tc.GetResponses(&Source{StudyID: res}, "formfoo", "token_2", 0)
+	cnf := &SourceConf{
+		Name:   "",
+		Source: "",
+		Config: []byte(`foo`),
+	}
+
+	events := tc.GetResponses(&Source{"mystudy", cnf}, "formfoo", "124121d12", 0)
+
 	e := Sliceit(events)
 
-	assert.Equal(t, 2, len(e))
+	assert.Equal(t, 4, len(e))
+	assert.Equal(t, "126", e[0].User.ID)
+	// assert.Equal(t, json.RawMessage([]byte(`"foo"`)), e[0].User.Metadata["key"])
+	assert.Equal(t, "127", e[1].User.ID)
+	// assert.Equal(t, json.RawMessage([]byte(`"bar"`)), e[1].User.Metadata["key"])
+	assert.Equal(t, "128", e[2].User.ID)
+	// assert.Equal(t, json.RawMessage([]byte(`"baz"`)), e[2].User.Metadata["key"])
 }
 
 func TestGetResponses_StartsFromOldIdxAndIterates(t *testing.T) {
 	res := resData("fly_example.json")
+	res2 := resData("fly_example2.json")
+
+	s := string(res)
+	data := GetResponsesResponse{}
+	json.Unmarshal([]byte(s), &data)
+
+	s2 := string(res2)
+	data2 := GetResponsesResponse{}
+	json.Unmarshal([]byte(s2), &data2)
 
 	ts, _ := TestServer(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Fin: %v\n", r.URL.Path)
@@ -136,34 +168,101 @@ func TestGetResponses_StartsFromOldIdxAndIterates(t *testing.T) {
 
 	tc := flyConnector{BaseUrl: ts.URL, Key: "sosecret", PageSize: 5}
 
-	events := tc.GetResponses(&Source{StudyID: res}, "formfoo", "token_3", 0)
+	events := tc.GetResponses(&Source{StudyID: res}, "formfoo", "9ba5db11ec6c63d22f08aade805bd363", 15)
 
 	e := Sliceit(events)
 
-	assert.Equal(t, 2, len(e))
+	assert.Equal(t, 16, e[0].Idx)
 
-	for i := 0; i < len(e); i++ {
-		assert.Equal(t, 1, e[0].Idx)
-		assert.Equal(t, 2, e[1].Idx)
+	lastIndex := len(e) - 1
+
+	// Last idx
+	// assert.Equal(t, "9ba5db11ec6c63d22f08aade805bd363", e[lastIndex].Pagination)
+	assert.Equal(t, "4viu4r8djwxwb2udbivx42avnawwj5wj", e[lastIndex].Pagination)
+
+	list1 := data.Items
+	list2 := data2.Items
+
+	list3 := list1
+
+	// Concatenation of items
+	for index := range list2 {
+		list3 = append(list3, list2[index])
 	}
+
+	s3 := string(res)
+	data3 := GetResponsesResponse{}
+	json.Unmarshal([]byte(s3), &data3)
+
+	assert.Condition(t, func() bool {
+
+		for i := range list3 {
+			// fmt.Println("list3[i].Token ->", list3[i].Token)
+
+			// Searches the entire array
+			if list3[i].Token == e[lastIndex].Pagination {
+
+				totalItems := len(list3)
+
+				// Iterate an array at a specific position "list3[5:8]"
+				startFromLastPosition := list3[i:totalItems]
+
+				fmt.Println("Remaining items:")
+				for v := range startFromLastPosition {
+					fmt.Println("startFromOldIdxAndIterates: ", startFromLastPosition[v].Token)
+				}
+				// Found!
+				return true
+			}
+		}
+		return false
+	}, "Idx does not match expected")
 
 }
 
 func TestValidateTokenIsSent(t *testing.T) {
 	res := resData("fly_example.json")
-	ts, _ := TestServer(func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Println("get token ->", r.Header.Get("Authorization"))
-		assert.Equal(t, "Bearer token_123", r.Header.Get("Authorization"))
+	type GetTokenResponse struct {
+		AccessToken string `json:"access_token"`
+	}
 
-		w.WriteHeader(200)
+	tt := flyConnector{
+		BaseUrl:      "",
+		Key:          "",
+		PageSize:     0,
+		ClientId:     "P0F1mNGTyOfAFEvgZEtjRHVBEEmzIyPi",
+		ClientSecret: "TXt1afXrWQitQxfKR4MwSdhPc5puWMiqXrv1Wmx64VluJM-xLgoFYfVa5neVieg5",
+	}
+	tokenResponse := tt.GetToken()
+
+	s := string(tokenResponse)
+	tokenStructure := GetTokenResponse{}
+	json.Unmarshal([]byte(s), &tokenStructure)
+
+	token := tokenStructure.AccessToken
+	tokenType := reflect.TypeOf(token)
+
+	ts, _ := TestServer(func(w http.ResponseWriter, r *http.Request) {
+		assert.Condition(t, func() bool {
+			// fmt.Println("token ->", token)
+			if token == "" {
+				return token != ""
+			}
+			return true
+		}, "Token is required")
+
+		assert.Equal(t, reflect.TypeOf(""), tokenType)
+		assert.Equal(t, "Bearer "+token, r.Header.Get("Authorization"))
+
+		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprint(w, res)
 	})
 
-	tc := flyConnector{BaseUrl: ts.URL, Key: "token_123", PageSize: 10}
+	tc := flyConnector{BaseUrl: ts.URL, Key: token, PageSize: 10}
 	events := tc.GetResponses(&Source{StudyID: res}, "formfoo", "token_4", 0)
 
 	e := Sliceit(events)
 
-	assert.Equal(t, 2, len(e))
+	assert.Equal(t, 4, len(e))
 }
