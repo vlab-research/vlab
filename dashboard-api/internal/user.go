98f981aca174e37fc2ee6c1a8e156a6114e2eb10
Juan Carrillo <54687120+juanpc10@users.noreply.github.com>
2022-08-04 19:05:26 -0500

    Update user.go

--- dashboard-api/internal/user.go
+++ dashboard-api/internal/user.go
@@ -9,6 +9,10 @@ type UserRepository interface {
 	CreateUser(ctx context.Context, userId string) (User, error)
 }
 
+type SaveCredentials interface {
+	SaveCredentials(ctx context.Context, clientId string, clientSecret string) (User, error)
+}
+
 var ErrUserAlreadyExists = errors.New("User Already Exists")
 
 //go:generate mockery --case=snake --outpkg=storagemocks --output=platform/storage/storagemocks --name=UserRepository
