2518b4c2640cab63060d8c56aee08a170332713a
Juan Carrillo <54687120+juanpc10@users.noreply.github.com>
2022-08-08 19:28:52 -0500

    Credentials struct added

--- dashboard-api/internal/user.go
+++ dashboard-api/internal/user.go
@@ -3,6 +3,7 @@ package studiesmanager
 import (
 	"context"
 	"errors"
+	"time"
 )
 
 type UserRepository interface {
@@ -13,10 +14,22 @@ type SaveCredentialsFly interface {
 	SaveCredentialsFly(ctx context.Context, clientId string, nickname string) (User, error)
 }
 
+type GetCredentials interface {
+	GetCredentials(ctx context.Context, clientId string) (Credentials, error)
+}
+
 var ErrUserAlreadyExists = errors.New("User Already Exists")
 
 //go:generate mockery --case=snake --outpkg=storagemocks --output=platform/storage/storagemocks --name=UserRepository
 
 type User struct {
 	Id string `json:"id"`
+}
+
+type Credentials struct {
+	Userid  string
+	Entity  string
+	Key     string
+	Created time.Time
+	Details string
 }
