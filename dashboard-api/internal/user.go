9db31944e7473830488c04bfb138d3bd7f5511a5
Juan Carrillo <54687120+juanpc10@users.noreply.github.com>
2022-08-10 18:25:58 -0500

    SaveCredentialsTypeform added

--- dashboard-api/internal/user.go
+++ dashboard-api/internal/user.go
@@ -14,11 +14,16 @@ type SaveCredentialsFly interface {
 	SaveCredentialsFly(ctx context.Context, clientId string, nickname string) (User, error)
 }
 
+type SaveCredentialsTypeform interface {
+	SaveCredentialsTypeform(ctx context.Context, clientId string, nickname string) (User, error)
+}
+
 type GetCredentials interface {
 	GetCredentials(ctx context.Context, clientId string) (Credentials, error)
 }
 
 var ErrUserAlreadyExists = errors.New("User Already Exists")
+var ErrIdNotExists = errors.New("Id does not exist")
 
 //go:generate mockery --case=snake --outpkg=storagemocks --output=platform/storage/storagemocks --name=UserRepository
 
