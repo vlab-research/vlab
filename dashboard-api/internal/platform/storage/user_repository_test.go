57f31b110d743c6ab0068ca75514dce79c85abb3
Juan Carrillo <54687120+juanpc10@users.noreply.github.com>
2022-08-11 15:50:27 -0500

    Test_UserRepository_GetCredentials_Succeed added

--- dashboard-api/internal/platform/storage/user_repository_test.go
+++ dashboard-api/internal/platform/storage/user_repository_test.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	studiesmanager "github.com/vlab-research/vlab/dashboard-api/internal"
 )
 
 func Test_UserRepository_SaveCredentials_UnexpectedError_during_Query(t *testing.T) {
@@ -49,5 +53,33 @@ func Test_UserRepository_GetCredentials_UnexpectedError_during_Query(t *testing.
 
 	_, err = repo.GetCredentials(context.Background(), userId)
 
-	assert.Equal(t, errors.New("user with id 'auth0|47016c1dab79c900713937fa' not exists: unexpected-error"), err)
+	assert.Equal(t, errors.New("unexpected-error"), err)
+}
+
+func Test_UserRepository_GetCredentials_Succeed(t *testing.T) {
+	userId := "auth0|47016c1dab79c900713937fa"
+	entity := "entity_fake"
+	details := json.RawMessage(`{"first_name": "juancarrillodev"}`)
+	key := "key123"
+
+	db, sqlMock, err := sqlmock.New()
+	if err != nil {
+		fmt.Println("failed to open sqlmock database:", err)
+	}
+	defer db.Close()
+
+	rows := sqlMock.NewRows([]string{"user_id",
+		"entity",
+		"key",
+		"created",
+		"details",
+	}).
+		AddRow(userId, entity, key, time.Date(2022, time.August, 11, 15, 33, 49, 614333595, time.Local), details)
+
+	sqlMock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM credentials WHERE user_id = $1")).WithArgs(userId).WillReturnRows(rows)
+
+	repo := NewGetCredentials(db)
+	credentials, err := repo.GetCredentials(context.Background(), userId)
+	assert.NoError(t, err)
+	assert.Equal(t, studiesmanager.NewCredentials(userId, entity, key, details, time.Date(2022, time.August, 11, 15, 33, 49, 614333595, time.Local)), credentials)
 }
