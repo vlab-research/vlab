fbe01d2fb9de4bfe84eae28c82e6fae77758f7ee
Sophie <sophie@sssoja.com>
2022-08-18 15:41:28 +0200

    create helper function to merge connected and unconnected accounts

--- dashboard-frontend/src/helpers/arrays.spec.ts
+++ dashboard-frontend/src/helpers/arrays.spec.ts
@@ -1,4 +1,4 @@
-import { lastValue } from './arrays';
+import { lastValue, arrayMerge } from './arrays';
 
 describe('lastValue', () => {
   it('returns last value of a given array', () => {
@@ -9,5 +9,63 @@ describe('lastValue', () => {
 
   it('returns undefined when the given array is empty', () => {
     expect(lastValue([])).toBe(undefined);
+  });
+});
+
+describe('arrayMerge', () => {
+  it('returns arr1 if arr2 is empty', () => {
+    const arr1 = [{ name: 'foo' }, { name: 'bar' }, { name: 'baz' }];
+    const arr2 = [];
+    const res = arrayMerge(arr1, arr2, 'name');
+    expect(res).toEqual(arr1);
+  });
+
+  it('merges the properties of one matching array element into another', () => {
+    const arr1 = [{ name: 'foo' }, { name: 'bar' }, { name: 'baz' }];
+    const arr2 = [{ name: 'foo', data: 'abcd' }];
+
+    const expectation = [
+      { name: 'foo', data: 'abcd' },
+      { name: 'bar' },
+      { name: 'baz' },
+    ];
+    const res = arrayMerge(arr1, arr2, 'name');
+
+    expect(res).toStrictEqual(expectation);
+  });
+
+  it('merges the properties of two matching array elements into another', () => {
+    const arr1 = [{ name: 'foo' }, { name: 'bar' }, { name: 'baz' }];
+    const arr2 = [
+      { name: 'foo', data: 'abcd' },
+      { name: 'baz', data: '1234' },
+    ];
+
+    const expectation = [
+      { name: 'foo', data: 'abcd' },
+      { name: 'bar' },
+      { name: 'baz', data: '1234' },
+    ];
+    const res = arrayMerge(arr1, arr2, 'name');
+
+    expect(res).toStrictEqual(expectation);
+  });
+
+  it('merges the properties of three matching array elements into another', () => {
+    const arr1 = [{ name: 'foo' }, { name: 'bar' }, { name: 'baz' }];
+    const arr2 = [
+      { name: 'foo', data: 'abcd' },
+      { name: 'bar', data: '!"·$' },
+      { name: 'baz', data: '1234' },
+    ];
+
+    const expectation = [
+      { name: 'foo', data: 'abcd' },
+      { name: 'bar', data: '!"·$' },
+      { name: 'baz', data: '1234' },
+    ];
+    const res = arrayMerge(arr1, arr2, 'name');
+
+    expect(res).toStrictEqual(expectation);
   });
 });
--- dashboard-frontend/src/helpers/arrays.ts
+++ dashboard-frontend/src/helpers/arrays.ts
@@ -1,1 +1,28 @@
 export const lastValue = <T>(values: T[]): T => values[values.length - 1];
+
+interface Arr1 {
+  name: string;
+}
+
+interface Arr2 extends Arr1 {
+  data: string;
+}
+
+export const arrayMerge = (arr1: Arr1[], arr2: Arr2[], key: string) => {
+  if (arr2.length > 0) {
+    return arr1.map(el => matcher(el, arr2, key));
+  }
+  return arr1;
+};
+
+const matcher = (el: any, arr2: [], key: string) => {
+  for (let i = 0; i < arr2.length; i++) {
+    if (el[key] === arr2[i][key]) {
+      return arr2[i];
+    }
+    if (!el[key] === arr2[i][key]) {
+      return el;
+    }
+  }
+  return el;
+};
