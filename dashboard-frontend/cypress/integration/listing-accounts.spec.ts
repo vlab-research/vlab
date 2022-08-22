d5d09539f617da23a8e062cddfd9ca5ca859c04b
Sophie <sophie@sssoja.com>
2022-08-22 18:06:39 +0200

    remove tests for some and all connected accounts

--- dashboard-frontend/cypress/integration/listing-accounts.spec.ts
+++ dashboard-frontend/cypress/integration/listing-accounts.spec.ts
@@ -51,7 +51,7 @@ describe('Given an authenticated user', () => {
       cy.visit('/accounts');
     });
 
-    it('sees the data associated with the connected account', () => {
+    it('sees the data associated with a connected account', () => {
       cy.get('[data-testid="account-list-item"]')
         .eq(0)
         .should('contain', 'Fly');
@@ -75,122 +75,6 @@ describe('Given an authenticated user', () => {
       cy.get('[data-testid="connect-button-connect"]')
         .should('contain', 'connect')
         .should('have.length', 2);
-    });
-  });
-
-  describe('When the user has two connected accounts and visits the accounts page', () => {
-    beforeEach(() => {
-      const connectedAccounts = [
-        {
-          name: 'Fly',
-          authType: 'secret',
-          connectedAccount: {
-            createdAt: Date.now(),
-            credentials: {
-              clientId: '123456',
-              clientSecret: 'qwertyuiop',
-            },
-          },
-        },
-        {
-          name: 'Typeform',
-          authType: 'token',
-          connectedAccount: {
-            createdAt: Date.now() - 24 * 60 * 60 * 1000,
-            credentials: {
-              token: '!"·$%&/()',
-            },
-          },
-        },
-      ];
-
-      connectedAccounts.map(account => createAccountResource(server, account));
-
-      cy.visit('/accounts');
-    });
-
-    it('sees the data associated with the connected accounts', () => {
-      cy.get('[data-testid="account-list-item"]')
-        .eq(1)
-        .should('contain', 'Typeform');
-      cy.get('[data-testid="input-token-1"]').should('have.value', '!"·$%&/()');
-    });
-
-    it('sees an update button for each connected account', () => {
-      cy.get('[data-testid="connect-button-update"]')
-        .should('contain', 'update')
-        .should('have.length', 2);
-    });
-
-    it('sees a connect button for the unconnected account', () => {
-      cy.get('[data-testid="connect-button-connect"]')
-        .should('contain', 'connect')
-        .should('have.length', 1);
-    });
-  });
-
-  describe('When all accounts are connected and the user visits the homepage', () => {
-    beforeEach(() => {
-      const connectedAccounts = [
-        {
-          name: 'Fly',
-          authType: 'secret',
-          connectedAccount: {
-            createdAt: Date.now(),
-            credentials: {
-              clientId: '123456',
-              clientSecret: 'qwertyuiop',
-            },
-          },
-        },
-        {
-          name: 'Typeform',
-          authType: 'token',
-          connectedAccount: {
-            createdAt: Date.now() - 24 * 60 * 60 * 1000,
-            credentials: {
-              token: '!"·$%&/()',
-            },
-          },
-        },
-        {
-          name: 'Some other account',
-          authType: 'token',
-          connectedAccount: {
-            createdAt: new Date('20/08/2021').getTime(),
-            credentials: {
-              token: '=)(/&%$·"!',
-            },
-          },
-        },
-      ];
-
-      connectedAccounts.map(account => createAccountResource(server, account));
-
-      cy.visit('/accounts');
-    });
-
-    it('sees the data associated with the connected accounts', () => {
-      cy.get('[data-testid="account-list-item"]')
-        .eq(2)
-        .should('contain', 'Some other account');
-      cy.get('[data-testid="input-token-2"]').should(
-        'have.value',
-        '=)(/&%$·"!'
-      );
-    });
-
-    it('sees an update button for each connected account', () => {
-      cy.get('[data-testid="connect-button-update"]')
-        .should('contain', 'update')
-        .should('have.length', 3);
-    });
-
-    it('sees no connect buttons because all accounts are connected', () => {
-      cy.get('[data-testid="connect-button-update"]').should(
-        'not.contain',
-        'connect'
-      );
     });
   });
 });
