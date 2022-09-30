bb15e01c6af9368c1c5670a303564373de4fbb8a
Sophie <sophie@sssoja.com>
2022-09-30 14:01:09 +0200

    create component rendererbased on json config

--- dashboard-frontend/src/pages/NewStudyPage/NewStudyPage.tsx
+++ dashboard-frontend/src/pages/NewStudyPage/NewStudyPage.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { ExclamationCircleIcon } from '@heroicons/react/solid';
 import useCreateStudy from './useCreateStudy';
 import PageLayout from '../../components/PageLayout';
 import PrimaryButton from '../../components/PrimaryButton';
-import { classNames } from '../../helpers/strings';
+
+import { Renderer } from './Renderer';
+import { getConfig } from './config';
+import { useMemo, useState } from 'react';
 
 const NewStudyPage = () => (
   <PageLayout title={'New Study'} testId="new-study-page" showBackButton>
@@ -14,11 +15,26 @@ const NewStudyPage = () => (
 const PageContent = () => {
   const { isCreating, errorMessage, createStudy } = useCreateStudy();
 
-  const handleSubmitForm = (event: any) => {
-    event.preventDefault();
+  const [state, setState] = useState({
+    name: '',
+    objective: '',
+  });
 
-    createStudy({ name: event.target.elements.name.value });
+  const handleSubmitForm = (e: any) => {
+    e.preventDefault();
+    console.log('state', state);
+    createStudy({ name: e.target.elements.name.value });
   };
+
+  const handleOnChange = (field: any) => (event: any) => {
+    const { value } = event.target;
+    console.log(value);
+    setState(prevState => ({ ...prevState, [field]: value }));
+  };
+
+  const config = useMemo(() => {
+    return getConfig({ state, onChange: handleOnChange });
+  }, [state]);
 
   return (
     <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -38,10 +54,11 @@ const PageContent = () => {
             <div className="px-4 py-5 bg-white sm:p-6">
               <div className="grid grid-cols-6 gap-6">
                 <div className="col-span-6 sm:col-span-4">
-                  <NameInput errorMessage={errorMessage} />
+                  <Renderer config={config} />
                 </div>
               </div>
             </div>
+
             <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
               <PrimaryButton
                 type="submit"
@@ -57,37 +74,5 @@ const PageContent = () => {
     </div>
   );
 };
-
-const NameInput = ({ errorMessage }: { errorMessage?: string }) => (
-  <React.Fragment>
-    <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-      Name
-    </label>
-    <div className="relative">
-      <input
-        type="text"
-        name="name"
-        id="name"
-        className={classNames(
-          'mt-1 block w-full shadow-sm sm:text-sm rounded-md',
-          errorMessage
-            ? 'focus:ring-red-500 focus:border-red-500 border-red-300 text-red-900 pr-10'
-            : 'focus:ring-indigo-500 focus:border-indigo-500  border-gray-300'
-        )}
-        data-testid="new-study-name-input"
-      />
-      {errorMessage && (
-        <div className="absolute pointer-events-none inset-y-0 right-0 pr-3 flex items-center">
-          <ExclamationCircleIcon
-            className="h-5 w-5 text-red-500"
-            aria-hidden="true"
-          />
-        </div>
-      )}
-    </div>
-
-    <p className="mt-2 text-sm text-red-600 h-1">{errorMessage}</p>
-  </React.Fragment>
-);
 
 export default NewStudyPage;
