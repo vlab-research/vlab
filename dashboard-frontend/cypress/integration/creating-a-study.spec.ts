ccfa61b76f2cc9240d93bec8eb746f17c13f6ff9
Sophie <sophie@sssoja.com>
2022-10-10 19:21:44 +0200

    first pass for a reusable testing function

--- dashboard-frontend/cypress/integration/creating-a-study.spec.ts
+++ dashboard-frontend/cypress/integration/creating-a-study.spec.ts
@@ -1,4 +1,10 @@
+import Chance from 'chance';
+
 import { makeServer } from '../../src/server';
+import { getConfig } from './../../src/pages/NewStudyPage/getConfig';
+import { baseConfig } from './../../src/pages/NewStudyPage/baseConfig';
+
+const chance = Chance();
 
 describe('Given an authenticated user', () => {
   let server: ReturnType<typeof makeServer>;
@@ -13,14 +19,62 @@ describe('Given an authenticated user', () => {
 
   describe('When he visits New Study page and creates a Study successfully', () => {
     it.only("He's redirected to the Studies page which lists the created Study", () => {
-      const newStudyName = 'Example Study';
+      const objs = getConfig(baseConfig);
+
+      const getOptions = (index: number) => {
+        return objs[index].options;
+      };
+
       cy.visit('/new-study');
 
-      cy.get('[data-testid="new-study-name-input"]').type(newStudyName);
+      const name = 'Example Study';
+      const objective =
+        getOptions(1)[Math.floor(Math.random() * getOptions(1).length)];
+      const optimization_goal =
+        getOptions(2)[Math.floor(Math.random() * getOptions(2).length)];
+      const destination_type =
+        getOptions(3)[Math.floor(Math.random() * getOptions(3).length)];
+      const page_id = chance.guid();
+      const instagram_id = chance.fbid();
+      const min_budget = chance.floating({ min: 1, max: 10000 });
+      const opt_window = chance.floating({ min: 0, max: 14 });
+      const ad_account = chance.fbid();
+      const country = chance.country({ full: true });
+
+      const responses: any = {
+        name,
+        objective,
+        optimization_goal,
+        destination_type,
+        page_id,
+        instagram_id,
+        min_budget,
+        opt_window,
+        ad_account,
+        country,
+      };
+
+      const text = (obj: any, response: any) => {
+        cy.get(`[data-testid="new-study-${obj.name}-input"]`).type(response);
+      };
+
+      const select = (obj: any, response: any) => {
+        cy.get(`[data-testid="new-study-${obj.name}-input"]`).select(response);
+      };
+
+      for (let i = 0; i < objs.length; i++) {
+        objs[i].type === 'text'
+          ? text(objs[i], responses[objs[i].name])
+          : select(
+              objs[i],
+              responses[objs[i].name].name || responses[objs[i].name]
+            );
+      }
+
       cy.get('[data-testid="new-study-submit-button"]').click();
 
       cy.url().should('eq', `${Cypress.config().baseUrl}/`);
-      cy.contains(newStudyName);
+      cy.contains(name);
     });
   });
 
