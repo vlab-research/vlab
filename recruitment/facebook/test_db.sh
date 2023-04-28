7e2b7dc2bf24ce5baaf6ee9f8da5c07de6213fdb
Spazzy <brendankamp757@gmail.com>
2023-04-28 16:39:05 +0200

    chore: remove unused system dir
    Signed-off-by: Spazzy <brendankamp757@gmail.com>

--- recruitment/facebook/test_db.sh
+++
@@ -1,5 +0,0 @@
-docker stop vlab-recruitment-test && docker rm vlab-recruitment-test
-
-docker run --name vlab-recruitment-test -d -p 5433:26257 cockroachdb/cockroach:v21.1.7 start-single-node --insecure
-
-cat ../../devops/sql/* | docker run -i --net=host --rm cockroachdb/cockroach:v21.1.7 sql --insecure --host localhost --port 5433 --database test
