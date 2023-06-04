af57e6bd7ae76d0919938432e51524b788a6a6f8
Nandan Rao <nandanmarkrao@gmail.com>
2023-06-04 12:29:03 -0400

    feat: adopt: facebook api backoff only on certain error codes

--- adopt/adopt/facebook/api.py
+++ adopt/adopt/facebook/api.py
@@ -10,15 +10,17 @@ logging.getLogger("backoff").addHandler(logging.StreamHandler())
 
 INTERVAL = 5 * 60
 
-# TODO:
-# check status code
-# only retry on known codes
-# 17, user request limit reached
 
-# 368 - page blocked from sending messages...
+def fatal_code(e):
+    # Non-fatal error codes:
+    # 17, user request limit reached
+    # 368 - page blocked from sending messages (should be??)
+    return e.api_error_code() not in {17, 368}
 
 
-@backoff.on_exception(backoff.constant, FacebookRequestError, interval=INTERVAL)
+@backoff.on_exception(
+    backoff.constant, FacebookRequestError, interval=INTERVAL, giveup=fatal_code
+)
 def call(fn, *args, **kwargs):
     res = fn(**kwargs)
 
