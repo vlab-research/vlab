31101e1d835905bf3289a8d4bfed8e1734ba57a8
Nandan Rao <nandanmarkrao@gmail.com>
2023-12-31 16:27:05 -0500

    fix: dashboard: change optimization goals to reflect adsets

--- dashboard/src/fixtures/general/optimizationGoals.ts
+++ dashboard/src/fixtures/general/optimizationGoals.ts
@@ -1,16 +1,73 @@
 const optimizationGoals = [
-  { name: 'APP_INSTALLS', label: 'App Installs' },
-  { name: 'BRAND_AWARENESS', label: 'Brand Awareness' },
-  { name: 'CONVERSIONS', label: 'Conversions' },
-  { name: 'EVENT_RESPONSES', label: 'Event Responses' },
-  { name: 'LEAD_GENERATION', label: 'Lead Generation' },
-  { name: 'LINK_CLICKS', label: 'Link Clicks' },
-  { name: 'MESSAGES', label: 'Messages' },
-  { name: 'POST_ENGAGEMENT', label: 'Post Engagement' },
-  { name: 'PRODUCT_CATALOG_SALES', label: 'Product Catalog Sales' },
-  { name: 'REACH', label: 'Reach' },
-  { name: 'STORE_VISITS', label: 'Store Visits' },
-  { name: 'VIDEO_VIEWS', label: 'Video Views' },
-]
+  {
+    name: 'AD_RECALL_LIFT',
+    label: 'Ad recall lift',
+  },
+  {
+    name: 'APP_INSTALLS',
+    label: 'App installs',
+  },
+  {
+    name: 'CLICKS',
+    label: 'Clicks',
+  },
+  {
+    name: 'CONVERSATIONS',
+    label: 'Conversations',
+  },
+
+  {
+    name: 'ENGAGED_USERS',
+    label: 'Engaged users',
+  },
+  {
+    name: 'EVENT_RESPONSES',
+    label: 'Event responses',
+  },
+  {
+    name: 'IMPRESSIONS',
+    label: 'Impressions',
+  },
+  {
+    name: 'LEAD_GENERATION',
+    label: 'Lead generation',
+  },
+  {
+    name: 'LINK_CLICKS',
+    label: 'Link clicks',
+  },
+  {
+    name: 'OFFSITE_CONVERSIONS',
+    label: 'Offsite conversions',
+  },
+  {
+    name: 'PAGE_ENGAGEMENT',
+    label: 'Page engagement',
+  },
+  {
+    namE: 'PAGE_LIKES',
+    label: 'Page likes',
+  },
+  {
+    name: 'POST_ENGAGEMENT',
+    label: 'Post engagement',
+  },
+  {
+    name: 'REACH',
+    label: 'Reach',
+  },
+  {
+    name: 'LANDING_PAGE_VIEWS',
+    label: 'Landing Page Views',
+  },
+  {
+    name: 'THRUPLAY',
+    label: 'Thruplay',
+  },
+  {
+    name: 'VISIT_INSTAGRAM_PROFILE',
+    label: 'Visit instagram profile',
+  },
+];
 
 export default optimizationGoals;
