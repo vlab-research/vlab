2fe17e41ed81c142ff7784117939414024d55811
Nandan Rao <nandanmarkrao@gmail.com>
2025-05-09 01:36:32 -0400

    fix(adopt): add fb error code 2 to retries

--- adopt/adopt/facebook/api.py
+++ adopt/adopt/facebook/api.py
@@ -15,7 +15,7 @@ def fatal_code(e):
     # Non-fatal error codes:
     # 17, user request limit reached
     # 368 - page blocked from sending messages (should be??)
-    return e.api_error_code() not in {17, 368, 80004}
+    return e.api_error_code() not in {2, 17, 368, 80004}
 
 
 @backoff.on_exception(
